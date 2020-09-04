7a0dac42ce1b7835e84e32f339a3dfeb934011ab
Swapnil Sengupta <dodosp2001@gmail.com>
2020-09-04 21:13:40 +0530

    Add authors and search functionality

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const indexRouter = require('./routes/index.js');
+const authorRouter = require('./routes/authors.js');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -21,6 +24,7 @@ db.on('error', err => console.error(err));
 db.once('open', () => console.log('Connected to mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server running on port 3000!");
